2c2e6995a5d1f97f6b1a92e50e8b637efe5689f5
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-07-06 15:53:24 -0700

    pc - initial code from https://start.spring.io/

---
+++ src/main/java/edu/ucsb/cs156/demo/springReact02/SpringReact02Application.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs156.demo.springReact02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReact02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReact02Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ucsb/cs156/demo/springReact02/SpringReact02ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs156.demo.springReact02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReact02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
